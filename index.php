a9baffaed4d8504e31ccf61d77a5e7ffa1246f7d
shashank shashikant parab <sp2339@afsconnect2.njit.edu>
2017-10-04 00:34:18 -0400

    Initial commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo "The value of \$tar: ".$tar."<br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
-print_r($year)
+print_r($year);
 
-echo 'Hello';
+echo "Hello";
 ?>
